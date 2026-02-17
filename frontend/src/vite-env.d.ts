c3e81acc178d533585aef93067fe9e17842dd100
Justin <jmccabe1@ggc.edu>
2026-02-16 19:05:40 -0500

    fixed

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -1,6 +1,4 @@
 /// <reference types="vite/client" />
-<<<<<<< HEAD
-=======
 
 interface ImportMetaEnv {
   readonly VITE_SUPABASE_URL: string
@@ -10,4 +8,3 @@ interface ImportMetaEnv {
 interface ImportMeta {
   readonly env: ImportMetaEnv
 }
->>>>>>> f76c1c5a47901a3cae46fd96bcd53b22c911863b
